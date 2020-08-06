8162a1238c6fb072da146b533a022381a2b94cf2
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-06 17:05:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(chocolateBars, element){return [element, ...chocolateBars]}
-function destructivelyAddElementToBeginningOfArray(chocolateBars, element){return chocolateBars.unshift(element)}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element){chocolateBars.unshift(element); return chocolateBars}
 function addElementToEndOfArray(chocolateBars, element){return[...chocolateBars, element]}
-function destructivelyAddElementToEndOfArray(chocolateBars, element){return chocolateBars.push(element)}
+function destructivelyAddElementToEndOfArray(chocolateBars, element){chocolateBars.push(element); return chocolateBars}
